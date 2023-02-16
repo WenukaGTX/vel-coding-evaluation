41bc904797135da24c6f234edb1713ebe5ec3fb8
wenukagtx <wenukagtx@gmail.com>
2023-02-16 13:37:36 +0530

    error fixed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { Button } from 'antd';
 import 'antd/dist/reset.css';
 import logo from './logo.svg';
+import { Col, Row } from 'antd';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button type="primary">Button</Button>
-      </header>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-6'>Left side</div>
+          <div className='col-6'>Right side</div>
+        </div>
+      </div>
     </div>
   );
 }
